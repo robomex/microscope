1f74425508ae4647cbf472ccb28487696ffca273
robomex <atmorales@gmail.com>
2013-12-31 16:52:28 -0600

    through voting

--- server/fixtures.js
+++ server/fixtures.js
@@ -11,14 +11,14 @@ if (Posts.find().count() === 0) {
     profile: { name: 'Sacha Greif' }
   });
   var sacha = Meteor.users.findOne(sachaId);
-  
   var telescopeId = Posts.insert({
     title: 'Introducing Telescope',
     userId: sacha._id,
     author: sacha.profile.name,
     url: 'http://sachagreif.com/introducing-telescope/',
     submitted: now - 7 * 3600 * 1000,
-    commentsCount: 2
+    commentsCount: 2,
+    upvoters: [], votes: 0
   });
   
   Comments.insert({
@@ -43,7 +43,8 @@ if (Posts.find().count() === 0) {
     author: tom.profile.name,
     url: 'http://meteor.com',
     submitted: now - 10 * 3600 * 1000,
-    commentsCount: 0
+    commentsCount: 0,
+    upvoters: [], votes: 0
   });
   
   Posts.insert({
@@ -52,7 +53,8 @@ if (Posts.find().count() === 0) {
     author: tom.profile.name,
     url: 'http://themeteorbook.com',
     submitted: now - 12 * 3600 * 1000,
-    commentsCount: 0
+    commentsCount: 0,
+    upvoters: [], votes: 0
   });
   for (var i = 0; i < 10; i++) {
     Posts.insert({
@@ -61,7 +63,8 @@ if (Posts.find().count() === 0) {
       userId: sacha._id,
       url: 'http://google.com/?q=test-' + i,
       submitted: now - i * 3600 * 1000,
-      commentsCount: 0
+      commentsCount: 0,
+      upvoters: [], votes: 0
     });
   }
 }
